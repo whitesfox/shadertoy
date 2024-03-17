69659f86bdaf5d9bea53b2b8245eab801507f272
real <hunkfuty@gamil.com>
2024-03-18 08:53:30 +0900

    feat:noiseShadertexture

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useState, useEffect, useMemo, useRef } from "react";
+import { useTexture } from "@react-three/drei";
+import * as THREE from "three";
+import Axios from "axios";
+
+const Scene = ({ vertex, fragment }) => {
+  const meshRef = useRef();
+  const noiseTexture = useTexture("/noise2.png");
+  useFrame((state) => {
+    let time = state.clock.getElapsedTime();
+    meshRef.current.material.uniforms.iTime.value = time + 20;
+  });
+  const uniforms = useMemo(
+    () => ({
+      iTime: {
+        type: "f",
+        value: 1.0,
+      },
+      iResolution: {
+        type: "v2",
+        value: new THREE.Vector2(4, 3),
+      },
+      iChannel0: {
+        type: "t",
+        value: noiseTexture,
+      },
+    }),
+    []
+  );
+  return (
+    <mesh ref={meshRef}>
+      <planeGeometry args={[4, 3]} />
+      <shaderMaterial
+        uniforms={uniforms}
+        vertexShader={vertex}
+        fragmentShader={fragment}
+        side={THREE.DoubleSide}
+      />
+    </mesh>
+  );
+};
 
 function App() {
+  const [vertex, setVertex] = useState("");
+  const [fragment, setFragment] = useState("");
+
+  useEffect(() => {
+    Axios.get("/vertexShader.glsl")
+      .then((res) => {
+        setVertex(res.data);
+        console.log(res.data);
+      })
+      .catch();
+    Axios.get("/fragmentShader.glsl").then((res) => setFragment(res.data));
+  }, []);
+
+  if (vertex == "" || fragment == "") return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas style={{ width: "100vw", height: "100vh" }}>
+      <Scene vertex={vertex} fragment={fragment} />
+    </Canvas>
   );
 }
 
